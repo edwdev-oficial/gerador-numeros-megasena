5de38e4bc6a94fa13e97fa8cc651a729cf85c52a
Eduardo <edjdevel@gmail.com>
2022-02-21 17:07:51 -0300

    create router.post in numbersContoller.js

--- api-gerador-numeros-mega-sena/src/controllers/numbersController.js
+++ api-gerador-numeros-mega-sena/src/controllers/numbersController.js
@@ -3,15 +3,28 @@ const Numbers = require('../models/numbers');
 const router = express.Router();
 
 // create
+router.post('/', async(req, res) => {
 
+    try {
 
+        const numbers = await Numbers.create(req.body);
+
+        return res.send(numbers);
+
+    }catch(error) {
+        return res.status(400).send({ error: 'Registration failed' });
+    };
+
+});
 
 // read
 router.get('/', async(req, res) => {
 
     try {
 
-        return res.send({ msg: 'Entrou na rota get' });
+        const numbers = await Numbers.find({});
+
+        return res.send(numbers);
 
     }catch(error) {
         return res.status(400).send({ error: 'List no found' });
