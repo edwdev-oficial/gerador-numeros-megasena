5bea1a4ebd9f9db4e27aed862c423ccdcf64d7a3
Eduardo <edjdevel@gmail.com>
2022-02-23 15:39:12 -0300

    model apostas.js craated

---
+++ app-gerador-numeros-mega-sena/model/apostas.js
@@ -0,0 +1,19 @@
+import { Config } from "../config/config.js";
+
+const url = Config.Url();
+
+export class Apostas {
+
+    static async getApostas(_data) {
+
+        const response = await fetch(`${url}/aposta`, {
+            method: 'POST',
+            body: JSON.stringify(_data),
+            headers: { 'Content-Type': 'application/json; charset=UTF-8' }
+        }); 
+
+        return response.json()
+
+    };
+
+};
