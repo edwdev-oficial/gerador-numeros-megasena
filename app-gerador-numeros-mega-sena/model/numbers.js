241747fcfae159286fef8f16c92d588a0c08bb64
Eduardo <edjdevel@gmail.com>
2022-02-23 13:04:28 -0300

    model numbers.js creaated in app

--- app-gerador-numeros-mega-sena/model/numbers.js
+++ app-gerador-numeros-mega-sena/model/numbers.js
@@ -1,23 +1,35 @@
 import { Config } from "../config/config.js";
 
-const port = Config.getPort();
+const url = Config.Url();
 
 export class Numbers {
 
     static async createNumbers(_data) {
 
-        const url = `http://localhost:${port}/numbers`
+        // const url = `http://localhost:${port}/numbers`
 
-        const response = await fetch(url, {
+        const response = await fetch(`${url}/numbers`, {
             method: 'POST',
             body: JSON.stringify(_data),
-            Headers: { 'Content-Type': 'application/json; charset=UTF-8' }
+            headers: { 'Content-Type': 'application/json; charset=UTF-8' }
         });
 
         return response.json();
 
     }
 
+    static async getNumbers(_data) {
+
+        const response = await fetch(`${url}/numbers/user`, {
+            method: 'POST',
+            body: JSON.stringify(_data),
+            headers: { 'Content-Type': 'application/json; charset=UTF-8' }
+        });
+
+        return response.json();
+
+    };
+
 };
 
-const metodo = new Numbers();
+// const metodo = new Numbers();
