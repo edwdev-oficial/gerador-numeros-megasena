08cf421cafc79f49b5408d4a142c6645d56a2da6
Eduardo <edjdevel@gmail.com>
2022-02-23 17:09:01 -0300

    model resultado created in app

--- app-gerador-numeros-mega-sena/model/resultado.js
+++ app-gerador-numeros-mega-sena/model/resultado.js
@@ -2,13 +2,8 @@ export class Resultado {
 
     static async getResultado(_data) {
 
-        console.log(_data.nomeLoteria)
-        console.log(_data.token)
-
         // const url = `https://apiloterias.com.br/app/resultado?loteria=[${_data.nomeLoteria}]&token=[${_data.token}]`
         const url = `https://apiloterias.com.br/app/resultado?loteria=${_data.nomeLoteria}&token=${_data.token}`
-
-        console.log(url)
 
         const response = await fetch(url);
 
